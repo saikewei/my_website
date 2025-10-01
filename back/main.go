7602431c7c5705e4d68a71fe77945e89b27e13fc
saikewei <huangyicheng0302@qq.com>
2025-10-02 00:23:27 +0800

    bug：没有加载config

--- back/main.go
+++ back/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/saikewei/my_website/back/internal/config"
 	"github.com/saikewei/my_website/back/internal/database"
 	"github.com/saikewei/my_website/back/photo"
 )
 
 func main() {
+	config.LoadConfig()
+
 	database.Connect()
 
 	r := gin.Default()
